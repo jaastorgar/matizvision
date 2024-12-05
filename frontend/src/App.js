7179cdd729532d3e7c849e4e201051abcafbfaa1
“jaastorgar” <ja.astorgar@duocuc.cl>
2024-12-05 12:10:48 -0300

    Confi Frontend.

---
+++ frontend/src/App.js
@@ -0,0 +1,17 @@
+import React from 'react';
+import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
+import Home from './pages/Home';
+import Login from './pages/Login';
+import Register from './pages/Register';
+
+const App = () => (
+  <Router>
+    <Routes>
+      <Route path="/" element={<Home />} />
+      <Route path="/login" element={<Login />} />
+      <Route path="/register" element={<Register />} />
+    </Routes>
+  </Router>
+);
+
+export default App;
